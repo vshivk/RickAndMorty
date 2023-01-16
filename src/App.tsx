3df2df9bda9162b1a38893caf3bc6bdb4d7c0ab3
victoria1900 <vshivkovavictoria@gmail.com>
2023-01-16 12:56:17 +0500

    init: created project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles/index.scss'
+import Header from "./components/header/header";
+
+const App = () => {
+    return (
+        <>
+            <Header/>
+        </>
+    );
+};
+
+export default App;
